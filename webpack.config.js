c62f75fd510f4eef4e496653a09699f9e94e9867
Dean Shaff <dean.shaff@gmail.com>
2020-10-20 12:33:26 -0700

    adding production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
 
+
+const mode = process.env.NODE_ENV === 'production' ? 'production': 'development'
+
+
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  mode: mode,
   module: {
     rules: [
       {
